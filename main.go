d07d3449c188536c17324802ad1711388c9c131a
Travis Huddleston <travis.huddleston@gmail.com>
2021-12-13 09:22:47 -0500

    day12 WIP

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import "advent-of-code-2021/day11"
+import "advent-of-code-2021/day12"
 
 func main() {
-	// day1.Run()
-	// day2.Run()
-	// day3.Run()
-	// day4.Run()
-	// day5.Run()
-	// day6.Run()
-	// day7.Run()
-	// day8.Run()
-	// day9.Run()
-	day11.Run()
+	day12.Run()
 }
